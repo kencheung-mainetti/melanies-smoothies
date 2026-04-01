c56a480b00510f228503bc90cbf870317a4c1081
kencheung-mainetti <ken.cheung@mainetti.com>
2026-04-01 21:59:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ if ingredients_list:
       
         #st.subheader(fruit_chosen + ' Nutrition Information')
         #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
   
     #st.write(ingredients_string)
